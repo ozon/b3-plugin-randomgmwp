a8491d5b27788ac250b74c154aed6e8216876221
Harry Gabriel <rootdesign@gmail.com>
2013-04-16 09:51:48 +0200

    add test for cmd

--- tests/test_randomgmwp.py
+++ tests/test_randomgmwp.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../extplugins'))
 from unittest import TestCase
 from mock import patch, call, Mock
 from mockito import when, verify
-from tests import Bf3TestCase
+from tests import Bf3TestCase, Mockito
 from b3.fake import FakeConsole, FakeClient
 from b3.config import XmlConfigParser, CfgConfigParser
 from b3.parsers.bf3 import GUNMASTER_WEAPONS_PRESET_BY_INDEX
@@ -35,6 +35,7 @@ gmwp: 20
         """)
         self.p = RgmwpPlugin(self.console, self.conf)
         self.p.onLoadConfig()
+        self.p.console.game.gameName = 'bf3'
         self.p.onStartup()
         self.superadmin.connects('superadmin')
 
@@ -59,3 +60,16 @@ gmwp: 20
         with patch.object(self.p, "setGunMasterWeaponPreset") as mock_setGunMasterWeaponPreset:
             self.p.setRandomRandomGunMasterWeaponPreset()
         self.assertTrue(mock_setGunMasterWeaponPreset.called)
+
+    def test_cmd_gmwp_random(self):
+        self.moderator.connects('moderator')
+        def getCvar_proxy(var_name):
+            if var_name == 'gunMasterWeaponsPreset':
+                return Cvar('gunMasterWeaponsPreset', value='1')
+            else:
+                return Mock()
+        self.p.console.getCvar = Mock(side_effect=getCvar_proxy)
+
+        self.p.setRandomRandomGunMasterWeaponPreset = Mock()
+        self.moderator.says('!gmwp random')
+        self.assertTrue(self.p.setRandomRandomGunMasterWeaponPreset.called)
